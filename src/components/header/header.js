6e5375af59fcec732833d900942e1b92cf013c5e
Dommaraju Sasi Kumar Raju <123371232+dskr1234@users.noreply.github.com>
2023-07-03 10:41:50 +0530

    Update header.js

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,45 +1,67 @@
-import {withRouter, Link} from 'react-router-dom'
+import {Link, withRouter} from 'react-router-dom'
 
 import Cookies from 'js-cookie'
 
-import './header.css'
+import {MdHome, MdWork} from 'react-icons/md'
+
+import {FiLogOut} from 'react-icons/fi'
+
+import './index.css'
 
 const Header = props => {
-  const {history} = props
-  const onLogout = () => {
+  const removeAccess = () => {
     Cookies.remove('jwt_token')
+    const {history} = props
     history.replace('/login')
   }
-
-  const navigateHome = () => {
-    history.replace('/')
-  }
-
   return (
-    <div className="header-container">
-      <div className="web-logo-container">
-        <button type="button" className="home" onClick={navigateHome}>
-          <img
-            src="https://assets.ccbp.in/frontend/react-js/logo-img.png"
-            alt="website logo"
-            className="website-logo"
-          />
-        </button>
-      </div>
-      <div className="links-container">
-        <Link to="/">
-          <p className="nav-link">Home</p>
-        </Link>
-        <Link to="/jobs">
-          <p className="nav-link">Jobs</p>
-        </Link>
-      </div>
-      <div className="logout-container">
-        <button type="button" className="logout-button" onClick={onLogout}>
+    <nav className="main-header-container">
+      <Link to="/">
+        <img
+          src="https://assets.ccbp.in/frontend/react-js/logo-img.png"
+          className="header-website-logo"
+          alt="website logo"
+        />
+      </Link>
+      <div className="lg-container">
+        <ul className="lg-list">
+          <li className="header-item">
+            <Link to="/" className="link">
+              Home
+            </Link>
+          </li>
+          <li className="header-item">
+            <Link to="/jobs" className="link">
+              Jobs
+            </Link>
+          </li>
+        </ul>
+        <button
+          type="submit"
+          className="header-logout-button"
+          onClick={removeAccess}
+        >
           Logout
         </button>
       </div>
-    </div>
+      <ul className="sm-container">
+        <li className="sm-list-item">
+          <Link to="/" className="l">
+            <MdHome className="icons" />
+          </Link>
+        </li>
+        <li className="sm-list-item">
+          <Link to="/jobs" className="l">
+            <MdWork className="icons" />
+          </Link>
+        </li>
+        <li className="sm-list-item">
+          <button type="submit" className="button-icon" onClick={removeAccess}>
+            <FiLogOut className="icons" />
+          </button>
+        </li>
+      </ul>
+    </nav>
   )
 }
 
